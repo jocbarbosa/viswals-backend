7c671470d79c8ad2b273d53b789bb1627e9072fd
Joao Barbosa <joaao@outlook.com>
2024-10-24 20:23:23 +0100

    setup swagger

--- internals/application/api/routes.go
+++ internals/application/api/routes.go
@@ -4,13 +4,22 @@ import (
 	"net/http"
 
 	"github.com/jocbarbosa/viswals-backend/internals/application/controllers"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-// NewRouter sets up the API routes using chi
+// NewRouter sets up the API routes
 func NewRouter(userController *controllers.UserController) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/users", userController.GetUsers)
+
+	mux.HandleFunc("/docs/swagger.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./docs/swagger.yaml")
+	})
+
+	mux.Handle("/docs/", httpSwagger.Handler(
+		httpSwagger.URL("/docs/swagger.yaml"),
+	))
 
 	return mux
 }
